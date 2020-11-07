6ac3504d7f1515b88eadef2ee4b5c07e6d17c7ef
Animesh Dhakal <dhakalanimesh385@gmail.com>
2020-11-07 13:07:48 +0545

    Fixed Some issued

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <ESP8266WiFi.h>
-#include <LittleFS.h>
 #define BLYNK_PRINT Serial
+#include <LittleFS.h>
 #include <BlynkSimpleEsp8266.h>
-
-
+#include <ArduinoJson.h>
 
 #define R1 D5
 #define R2 D6
@@ -15,37 +14,104 @@
 #define S3 D3
 #define S4 D4
 
-unsigned int time_now = 0;
 int mode=0;
+int write_mode=1;
+unsigned int time_now=0;
 
 int flag1=0;
 int flag2=0;
 int flag3=0;
 int flag4=0;
 
-char ssid[] = "unique nepal";
-char pass[] = "UNIQUENEPAL";
-char auth[] = "nlJHegSIKdIJWqf66_0KqnvuFVIz_8qM";
+int st1=0;
+int st2=0;
+int st3=0;
+int st4=0;
+
+char ssid[]="unique nepal";
+char pass[]="UNIQUENEPAL";
+char auth[]="nlJHegSIKdIJWqf66_0KqnvuFVIz_8qM";
+
+void writeBlynk(){
+  File file = LittleFS.open("./config.json", "r");
+  size_t size = file.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  file.readBytes(buf.get(), size);
+  file.close();
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, buf.get());
+  Blynk.virtualWrite(V1, doc["R1"].as<int>());
+  Blynk.virtualWrite(V2, doc["R2"].as<int>());
+  Blynk.virtualWrite(V3, doc["R3"].as<int>());
+  Blynk.virtualWrite(V4, doc["R4"].as<int>());
+  
+}
+
 
 BLYNK_CONNECTED(){
-  Serial.println("Connected to the Server");
-  Blynk.syncVirtual(V1, V2, V3, V4);
+  Serial.println("Connected to the server");
+  writeBlynk();
+  Blynk.syncVirtual(V1,V2,V3,V4);
+  write_mode=0;
 }
 BLYNK_WRITE(V1){
-  digitalWrite(R1, param.asInt());
-}
-BLYNK_WRITE(V2){
-  digitalWrite(R2, param.asInt());
-}
-BLYNK_WRITE(V3){
-  digitalWrite(R3, param.asInt());
-}
-BLYNK_WRITE(V4){
-  digitalWrite(R4, param.asInt());
+  digitalWrite(R1, st1=param.asInt());
 }
 
-void with_internet(){
-  Serial.println("With Internet");
+BLYNK_WRITE(V2){
+  digitalWrite(R2, st2=param.asInt());
+}
+
+BLYNK_WRITE(V3){
+  digitalWrite(R3, st3=param.asInt());
+}
+BLYNK_WRITE(V4){
+  digitalWrite(R4, st4=param.asInt());
+}
+
+void readFS(){
+  File file = LittleFS.open("./config.json", "r");
+  while (file.available())
+  {
+    Serial.write(file.read());
+  }
+  
+  if(!file){
+    file.close();
+    File file = LittleFS.open("./config.json", "w");
+    file.write("{\"R1\":0,\"R2\":0,\"R3\":0,\"R4\":0}");
+  }
+  size_t size = file.size();
+  std::unique_ptr<char[]> buf(new char[size]);
+  file.readBytes(buf.get(), size);
+  file.close();
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, buf.get());
+  digitalWrite(R1, doc["R1"].as<int>());
+  digitalWrite(R2, doc["R2"].as<int>());
+  digitalWrite(R3, doc["R3"].as<int>());
+  digitalWrite(R4, doc["R4"].as<int>());
+}
+
+void writeFS(){
+  File file = LittleFS.open("./config.json", "w");
+  StaticJsonDocument<200> doc;
+  doc["R1"]=st1;
+  doc["R2"]=st2;
+  doc["R3"]=st3;
+  doc["R4"]=st4;
+  serializeJson(doc, file);
+  file.close();
+}
+void checkInternet(){
+  if(millis()>=time_now+3000){
+    time_now+=3000;
+    if(Blynk.connected()){mode=0;}else{mode=1;};
+  }
+}
+
+void withInternet(){
+  
   if (digitalRead(S1) == LOW)
   {
     if (flag1 == 0)
@@ -119,20 +185,12 @@ void with_internet(){
     }
   }
 }
-
-void without_internet(){
-  Serial.println("No internet");
+void withoutInternet(){
+  Serial.println("Without internet");
 }
-void checkinternet(){
-  if(millis()>=time_now+3000){
-    time_now+=3000;
-    if(Blynk.connected()){mode=0;}else{mode=1;};
-  }
-}
-
-
 void setup(){
   Serial.begin(115200);
+  LittleFS.begin();
   pinMode(R1, OUTPUT);
   pinMode(R2, OUTPUT);
   pinMode(R3, OUTPUT);
@@ -142,21 +200,22 @@ void setup(){
   pinMode(S2, INPUT_PULLUP);
   pinMode(S3, INPUT_PULLUP);
   pinMode(S4, INPUT_PULLUP);
-  
+  readFS();
   WiFi.begin(ssid, pass);
-  while(WiFi.status()!=WL_CONNECTED){
+  while (WiFi.status()!=WL_CONNECTED)
+  {
     Serial.print("*");
     delay(100);
   }
-  Serial.println("Connected to the server");
   Blynk.config(auth);
   
-
-
 }
 
 void loop(){
   Blynk.run();
-  checkinternet();
-  if(mode==0){with_internet();}else{without_internet();}
+  checkInternet();
+  if(mode==0){withInternet();}else{withoutInternet();}
+  if(write_mode==0){writeFS();}
+  Serial.println(st1);
+  
 }
