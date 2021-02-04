47ef8b01c36bb4284c409a2919668a35030fdf08
Animeshdhakal <dhakalanimesh385@gmail.com>
2021-02-04 17:27:59 -0500

    More installation Script fix

--- build/get.py
+++ build/get.py
@@ -1,10 +1,31 @@
 import os, sys
-import requests, json
 
 def help():
-    p = f'Use "python{sys.version_info[0]} ./build/get.py install" for installing all requirements'
-    print(p)
+    print("""
+        Lists of commands :
+        
+        `python3 build/get.py install` for installation,
 
+        `python3 build/get.py reinstallation` for reinstallation,
+
+        `python3 build/get.py update` for updating to desired version
+
+        This Script is Made only for HomeAutomation Project
+        Made by: Mr. Animesh Dhakal
+    """)
+
+def cmd_install():
+    ci = f"""
+        echo "Installing Requirements for HomeAutomation" \n
+        git submodule update --init \n
+        cd ./build/esp8266/ \n
+        git checkout tags/2.7.4 \n
+        echo "Installing Requirements for Esp8266"
+        git submodule update --init \n
+        cd tools \n
+        python{sys.version_info[0]} get.py \n
+    """
+    os.system(ci)
 
 def update():
     ver = input("Enter the version you want:  ")
@@ -29,25 +50,22 @@ def update():
 
 def install():
     if not os.listdir("./build/esp8266"):
-        c = f"""
-            echo "Installing Requirements for HomeAutomation"
-            git submodule update --init \n
-            cd ./build/esp8266/ \n{sys.version_info[0]}
-            git checkout tags/2.7.4 \n
-            echo "Installing Requirements for Esp8266"
-            git submodule update --init \n
-            cd tools \n
-            python{sys.version_info[0]} get.py \n
-        """
-        os.system(c)
+        cmd_install();
 
     else:
         print("Already Installed")
+
+def reinstall():
+    if input("Are you sure you (y/n): ") == 'y':
+        print("Starting Reinstallation")
+        os.system("rm -rf ./build/esp8266 \n rm -rf ./build/makeEspArduino \n rm -rf ./.git/modules \n")
+        cmd_install()
 
 def main():
     if(sys.argv[1]):
         if sys.argv[1] == 'install': install()
         elif sys.argv[1] == 'update': update()
+        elif sys.argv[1] == 'reinstall': reinstall()
         else: help()
     else:
         help()
